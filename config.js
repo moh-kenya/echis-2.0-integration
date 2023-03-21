63476f128fdaf2ff10658c24c6216463ac19855e
Njuguna Ndung'u <njgndungu@yahoo.com>
2023-03-21 11:38:27 +0300

    register the mediator on OpenHIM

---
+++ config.js
@@ -0,0 +1,10 @@
+const OPENHIM = {
+  username: process.env.OPENHIM_USERNAME || "interop@openhim.org",
+  password: process.env.OPENHIM_PASSWORD || "interop-password",
+  apiURL: process.env.OPENHIM_API_URL || "https://openhim-core:8080",
+  trustSelfSigned: true,
+}
+
+module.exports = {
+  OPENHIM,
+}
--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const bodyParser = require('body-parser');
-const express = require('express')
-const app = express()
-const registryRoutes = require('./src/routes/client');
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const { registerMediator } = require("openhim-mediator-utils");
+const { OPENHIM } = require("./config");
+const registryRoutes = require("./src/routes/client");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/client-registry', registryRoutes);
-app.use('/service-request', serviceRequestRoutes);
-app.use('/community-referal', communityReferalRoutes);
+app.use("/client-registry", registryRoutes);
+app.use("/service-request", serviceRequestRoutes);
+app.use("/community-referal", communityReferalRoutes);
 
-const PORT = 9000
+const PORT = 9000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Loaded');
+const registerMediatorCallback = (err) => {
+  if (err) {
+    throw new Error(`Mediator Registration Failed: Reason ${err}`);
+  }
+
+  logger.info("Successfully registered mediator.");
+};
+
+registerMediator(OPENHIM, mediatorConfig, registerMediatorCallback);
+
+app.get("/", (req, res) => {
+  res.send("Loaded");
 });
