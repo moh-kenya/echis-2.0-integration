972e5263face664bfbac44c14b1d45cebacb87e6
Njuguna Ndung'u <njgndungu@yahoo.com>
2023-03-22 18:08:19 +0300

    add basic error handling

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -11,18 +11,26 @@ const axiosInstance = axios.create({
 });
 
 const searchClientByIdType = async (echisClientDoc) => {
-  // const idInformation = getIdInfoFromEchisPayload(echisClientDoc);
-  // const idType = idInformation.identificationType;
-  // const idNumber = idInformation.identificationNumber
-  const res = await axiosInstance.get(
-    `partners/registry/search/${echisClientDoc?.identifications[0]?.identificationType}/${echisClientDoc?.identifications[0]?.identificationNumber}`
-  );
-  if (res.data.clientExists) {
-    return res.data.clientExists && res.data.client.clientNumber;
-  } else {
-    const response = await updateEchisClient(echisClientDoc);
-    console.log(response);
-    return response;
+  try {
+    let identificationType;
+    if (echisClientDoc?.identifications[0]?.identificationType === 'national_id') {
+      identificationType = 'national_id';
+    } else {
+      identificationType = echisClientDoc?.identifications[0]?.identificationType;
+    }
+    const res = await axiosInstance.get(
+      `partners/registry/search/${identificationType}/${echisClientDoc?.identifications[0]?.identificationNumber}`
+    );
+    if (res.data.clientExists) {
+      return res.data.clientExists && res.data.client.clientNumber;
+    } else {
+      const response = await updateEchisClient(echisClientDoc);
+      console.log(response);
+      return response;
+    }
+  } catch (error) {
+    console.error(error);
+    return error;
   }
 };
 
