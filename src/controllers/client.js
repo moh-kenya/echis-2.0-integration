d6a6ef3e0f765f600d2091dd745ba3a93657f269
Njuguna Ndung'u <njgndungu@yahoo.com>
2023-03-22 18:15:36 +0300

    use object and not array

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -13,10 +13,10 @@ const axiosInstance = axios.create({
 const searchClientByIdType = async (echisClientDoc) => {
   try {
     let identificationType;
-    if (echisClientDoc?.identifications[0]?.identificationType === 'national_id') {
+    if (echisClientDoc?.identifications?.identificationType === 'national_id') {
       identificationType = 'national_id';
     } else {
-      identificationType = echisClientDoc?.identifications[0]?.identificationType;
+      identificationType = echisClientDoc?.identifications?.identificationType;
     }
     const res = await axiosInstance.get(
       `partners/registry/search/${identificationType}/${echisClientDoc?.identifications[0]?.identificationNumber}`
