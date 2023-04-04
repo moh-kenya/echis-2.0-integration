bb3698632de7d50742533bf7d547d99347bfc91f
kitsao <kittemm@gmail.com>
2023-04-04 04:26:34 +0300

    chore(#22): linting rules

--- src/routes/aggregate.js
+++ src/routes/aggregate.js
@@ -1,11 +1,11 @@
-const { Router } = require('express');
-const { getMoh515Data } = require('../controllers/aggregate');
+const {Router} = require('express');
+const {getMoh515Data} = require('../controllers/aggregate');
 
 const router = Router();
 
 router.get('/aggregate',
   async (req, res) => {
     await getMoh515Data(req, res);
-});
+  });
 
 module.exports = router;
