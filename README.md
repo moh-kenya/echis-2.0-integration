866105a9ad65d4ddf85251e882ab01edce9b025e
karimkkanji <kanji.karim@taifapay.go.ke>
2023-04-12 12:29:30 +0300

    UPD: added postman collection and updated links

--- README.md
+++ README.md
@@ -40,5 +40,4 @@ nodemon ./test/mock-server/index.js
 
 # Postman Collection for Testing
 
-Here is a link to the postman collection.
-[Mock Server & Tests Postman Collection](https://api.postman.com/collections/18803995-ac4df61a-232a-416e-96c0-fa67881defa6?access_key=PMAT-01GXTB8F35GHEA074QM34347ES)
+The Postman collection can be imported from test/PostmanCollection folder
--- test/mock-server/index.js
+++ test/mock-server/index.js
@@ -7,88 +7,114 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/clientRegistry/", (req, res) => {
-  const mockedResponse = {
-    clientExists: true,
-    client: {
-      clientNumber: "MOH" + faker.random.alphaNumeric(10).toUpperCase(),
-      firstName: faker.name.firstName(),
-      middleName: faker.name.lastName(),
-      lastName: faker.name.lastName(),
-      dateOfBirth: faker.date.past().toISOString(),
-      maritalStatus: faker.helpers.arrayElement([
-        "single",
-        "married",
-        "divorced",
-      ]),
-      gender: faker.helpers.arrayElement([
-        "male",
-        "female",
-        "unspecified",
-        "intersex",
-      ]),
-      occupation: faker.name.jobTitle(),
-      religion: faker.helpers.arrayElement(["christian", "muslim", "hindu"]),
-      educationLevel: faker.helpers.arrayElement([
-        "primary",
-        "secondary",
-        "college",
-        "master's",
-        "doctorate",
-      ]),
-      country: faker.address.countryCode(),
-      countyOfBirth: faker.address.zipCode(),
-      isAlive: faker.datatype.boolean(),
-      originFacilityKmflCode: faker.random.alphaNumeric(5),
-      isOnART: faker.datatype.boolean(),
-      nascopCCCNumber: null,
-      residence: {
-        county: faker.address.zipCode(),
-        subCounty: faker.address.city(),
-        ward: faker.address.street(),
-        village: faker.address.street(),
-        landMark: faker.address.street(),
-        address: faker.address.streetAddress(),
+app.get("/partners/registry/search/:idType/:idNumber", (req, res) => {
+  const idType = req.params.idType;
+  const idNumber = req.params.idNumber;
+  if (idNumber === "test-not-found") {
+    res.status(400).json({
+      clientExists: false,
+      client: {
+        clientNumber: null,
+        firstName: null,
+        middleName: null,
+        lastName: null,
+        dateOfBirth: null,
+        maritalStatus: null,
+        gender: null,
+        occupation: null,
+        religion: null,
+        educationLevel: null,
+        country: null,
+        countyOfBirth: null,
+        isAlive: false,
+        originFacilityKmflCode: null,
+        isOnART: false,
+        nascopCCCNumber: null,
+        residence: null,
+        identifications: [],
+        contact: null,
+        nextOfKins: [],
       },
-      identifications: [
-        {
-          countryCode: faker.address.countryCode(),
-          identificationType: faker.helpers.arrayElement([
-            "national-id",
-            "passport",
-            "driver-license",
-          ]),
-          identificationNumber: faker.random.alphaNumeric(10),
+    });
+  } else {
+    const mockedResponse = {
+      clientExists: true,
+      client: {
+        clientNumber: "MOH" + faker.random.alphaNumeric(10).toUpperCase(),
+        firstName: faker.name.firstName(),
+        middleName: faker.name.lastName(),
+        lastName: faker.name.lastName(),
+        dateOfBirth: faker.date.past().toISOString(),
+        maritalStatus: faker.helpers.arrayElement([
+          "single",
+          "married",
+          "divorced",
+        ]),
+        gender: faker.helpers.arrayElement([
+          "male",
+          "female",
+          "unspecified",
+          "intersex",
+        ]),
+        occupation: faker.name.jobTitle(),
+        religion: faker.helpers.arrayElement(["christian", "muslim", "hindu"]),
+        educationLevel: faker.helpers.arrayElement([
+          "primary",
+          "secondary",
+          "college",
+          "master's",
+          "doctorate",
+        ]),
+        country: faker.address.countryCode(),
+        countyOfBirth: faker.address.zipCode(),
+        isAlive: faker.datatype.boolean(),
+        originFacilityKmflCode: faker.random.alphaNumeric(5),
+        isOnART: faker.datatype.boolean(),
+        nascopCCCNumber: null,
+        residence: {
+          county: faker.address.zipCode(),
+          subCounty: faker.address.city(),
+          ward: faker.address.street(),
+          village: faker.address.street(),
+          landMark: faker.address.street(),
+          address: faker.address.streetAddress(),
         },
-      ],
-      contact: {
-        primaryPhone: faker.phone.number(),
-        secondaryPhone: faker.phone.number(),
-        emailAddress: faker.internet.email(),
-      },
-      nextOfKins: [
-        {
-          name: faker.name.findName(),
-          relationship: faker.helpers.arrayElement([
-            "spouse",
-            "parent",
-            "sibling",
-            "child",
-          ]),
-          residence: faker.address.streetAddress(),
-          contact: {
-            primaryPhone: faker.phone.number(),
-            secondaryPhone: faker.phone.number(),
-            emailAddress: faker.internet.email(),
+        identifications: [
+          {
+            countryCode: faker.address.countryCode(),
+            identificationType: idType,
+            identificationNumber: idNumber,
           },
+        ],
+        contact: {
+          primaryPhone: faker.phone.number(),
+          secondaryPhone: faker.phone.number(),
+          emailAddress: faker.internet.email(),
         },
-      ],
-    },
-  };
+        nextOfKins: [
+          {
+            name: faker.name.findName(),
+            relationship: faker.helpers.arrayElement([
+              "spouse",
+              "parent",
+              "sibling",
+              "child",
+            ]),
+            residence: faker.address.streetAddress(),
+            contact: {
+              primaryPhone: faker.phone.number(),
+              secondaryPhone: faker.phone.number(),
+              emailAddress: faker.internet.email(),
+            },
+          },
+        ],
+      },
+    };
 
-  res.json(mockedResponse);
+    res.json(mockedResponse);
+  }
 });
-app.post("/api/clientRegistry/", (req, res) => {
+app.post("/partners/registry/", (req, res) => {
   let mockedResponse = "";
   if (req.body.identifications.identificationNumber === "test") {
     mockedResponse = {
@@ -180,7 +206,7 @@ app.post("/api/clientRegistry/", (req, res) => {
     res.json(mockedResponse);
   }
 });
-app.post("/api/oauth/token/", (req, res) => {
+app.post("/connect/token", (req, res) => {
   // Generate the access token
   const accessToken = generateAccessToken();
 
