987e6e2221ab65818d6717ebf5c00aea53ebd161
kitsao <kittemm@gmail.com>
2023-04-13 07:22:00 +0300

    updated service request

--- src/utils/referral.js
+++ src/utils/referral.js
@@ -1,139 +1,185 @@
 require('dotenv/config');
-const axios = require('axios');
-const {FHIR} = require('../../config');
-const FHIR_URL = FHIR.url;
-
-const axiosInstance = axios.create({
-  baseURL: FHIR_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async function (error) {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      const token = await generateToken();
-      axiosInstance.defaults.headers.common[
-        'Authorization'
-      ] = `Bearer ${token}`;
-      return axiosInstance(originalRequest);
-    }
-    return Promise.reject(error);
-  }
-);
-
-const sampleEchisReferralPayload = {
-  upi: '854a32fb-50a5-4ade-9c75-fd6e9a88e572',
-  name: 'Ruth Melon',
-  form: 'pregnancy_home_visit',
-  has_lost_pregnancy_within_last_month: '',
-  danger_signs: 'very_pale getting_tired_easily breathlessness',
-  other_danger_sign: '',
-  takes_iron_or_folate_supplements: 'yes',
-  mental_danger_signs: 'staying_away_from_people tearfulness',
-  other_mental_danger_sign: '',
-  observed_mental_danger_signs: 'lack_of_hygiene anxiety irritability',
-  other_observed_mental_danger_signs: '',
-  is_still_pregnant: 'yes',
-  has_started_anc: '',
-  anc_upto_date: 'no',
-  has_lost_pregnancy_within_last_month_subsequent_visit: '',
-  pregnancy_test_result: '',
-  is_pregnant: '',
-  is_on_family_planning: '',
-  period_of_pregnancy: '',
-  known_lmp: '',
-  next_anc_visit_date: '',
-  reported_date: 1679413362551,
-  chu_code: '701583',
-  chu_name: 'Koluoch Community Health Unit',
-  referred_to_facility_code: '17345',
-  referred_to_facility_name: 'Ndege Oriedo Dispensary'
-};
-
-const serviceRequestPayload = {
-  resourceType: 'ServiceRequest',
-  status: 'active',
-  intent: 'order',
-  category: [
-    {
-      coding: [
-        {
-          system: 'http://nhdd-api.health.go.ke/orgs/MOH-KENYA/sources/nhdd/',
-          code: '28414',
-          display: 'Patient referred for medical consultation'
-        }
-      ],
-      text: 'Consultation'
-    }
-  ],
-  priority: 'urgent',
-  subject: {
-    reference: 'http://dhpstagingapi.health.go.ke/visit/registry/search/upi/MOH1664351915',
-    type: 'Patient',
-    display: 'MOH1664351915'
-  },
-  occurrencePeriod: {
-    resourceType: 'Period',
-    start: '2023-03-20',
-    end: '2023-03-23'
-  },
-  authoredOn: '2023-03-20',
-  requester: {
-    reference: 'http://nhdd-api.health.go.ke/orgs/MOH-KENYA/sources/nhdd/13023',
-    type: 'Organization',
-    display: '<chu code>'
-  },
-  performer: [
-    {
-      reference: 'http://nhdd-api.health.go.ke/orgs/MOH-KENYA/sources/nhdd/13023',
-      type: 'Organization',
-      display: '<facility kmfl code>'
-    }
-  ],
-  reasonCode: [
-    {
-      coding: [
-        {
-          system: 'http://nhdd-api.health.go.ke/orgs/MOH-KENYA/sources/nhdd/',
-          code: '9585',
-          display: 'Severe headache'
-        },
-        {
-          system: 'http://nhdd-api.health.go.ke/orgs/MOH-KENYA/sources/nhdd/',
-          code: '46048',
-          display: 'Decreased fetal movements'
-        }
-      ]
-    }
-  ],
-  note: [
-    {
-      text: 'Any additional text that needs to be sent'
-    }
-  ]
-};
+const { DateTime } = require('luxon');
+const { CLIENT_REGISTRY, NHDD, SNOMED_CT } = require('../../config');
+const NHDD_URL = `${NHDD.url}/orgs/MOH-KENYA/sources`;
+const SNOMED_CT_URL = SNOMED_CT.url;
+const CLIENT_REGISTRY_URL = `${CLIENT_REGISTRY.url}/partners/registry/search/upi`;
+const NHDD_GENERIC_PATH = `${NHDD_URL}/nhdd/concepts/`;
+const NHDD_KMHFL_PATH = `${NHDD_URL}/KMHFL/concepts`;
 
 const echisNHDDValuesCoding = {
-  // todo
+  vaginal_bleeding: {
+    system: NHDD_GENERIC_PATH,
+    code: `49168`,
+    display: `Vaginal Bleeding`
+  },
+  fits: {
+    system: NHDD_GENERIC_PATH,
+    code: `40356`,
+    display: `Fits`
+  },
+  severe_abdominal_pain: {
+    system: NHDD_GENERIC_PATH,
+    code: `18523`,
+    display: `Severe Abdominal Pain`
+  },
+  severe_headache: {
+    system: NHDD_GENERIC_PATH,
+    code: `9636`,
+    display: `Severe Headache`
+  },
+  very_pale: {
+    system: NHDD_GENERIC_PATH,
+    code: `21598`,
+    display: `Very Pale`
+  },
+  fever: {
+    system: NHDD_GENERIC_PATH,
+    code: `32879`,
+    display: `Fever`
+  },
+  reduced_or_no_fetal_movement: {
+    system: NHDD_GENERIC_PATH,
+    code: `46048`,
+    display: `Reduced or no fetal movement`
+  },
+  breaking_of_water : {
+    system: NHDD_GENERIC_PATH,
+    code: `32011`,
+    display: `Breaking of water`
+  },
+  getting_tired_easily: {
+    system: SNOMED_CT_URL,
+    code: `248268002`,
+    display: `Getting tired easily`
+  },
+  swelling_of_the_face_and_hands: {
+    system: NHDD_GENERIC_PATH,
+    code: `18838`,
+    display: `Swelling of the face and hands`
+  },
+  breathlessness: {
+    system: NHDD_GENERIC_PATH,
+    code: `5142`,
+    display: `Breathlessness`
+  },
+  none: {
+    system: NHDD_GENERIC_PATH,
+    code: `48590`,
+    display: `None`
+  },
+  other: {
+    system: NHDD_GENERIC_PATH,
+    code: `12737`,
+    display: `Other`
+  },
+  lack_of_sleep: {
+    system: NHDD_GENERIC_PATH,
+    code: `29541`,
+    display: `Lack of sleep`
+  },
+  feeling_of_worthlessness: {
+    system: SNOMED_CT_URL,
+    code: `247892001`,
+    display: `Feeling of worthlessness`
+  },
+  staying_away_from_people: {
+    system: NHDD_GENERIC_PATH,
+    code: `29644`,
+    display: `Staying away from people`
+  },
+  tearfulness: {
+    system: SNOMED_CT_URL,
+    code: `271951008`,
+    display: `Tearfulness`
+  },
+  lack_of_hygiene: {
+    system: NHDD_GENERIC_PATH,
+    code: `41667`,
+    display: `Lack of hygiene`
+  },
+  anxiety: {
+    system: NHDD_GENERIC_PATH,
+    code: `15111`,
+    display: `Anxiety/worried for no apparent reason`
+  },
+  irritability: {
+    system: NHDD_GENERIC_PATH,
+    code: `12218`,
+    display: `Irritability/agitated`
+  }
 };
+
+const extractNotes = (data) => {
+  const notes = {};
+  for (const [key, value] of Object.entries(data)) {
+    if(value){
+      notes[key] = value;
+    }
+  }
+  return [{text: `${JSON.stringify(notes)}`}];
+};
+
+const extractReasonCode = (data) => {
+  let coding = [];
+  const reasons = [...new Set((Object.values(data).map(str => str.split(' '))).flat())].filter(elem => elem !== `none`);
+  reasons.forEach(reason => coding.push(echisNHDDValuesCoding[reason]));
+  return [{coding: coding}];
+};
+
+const status = [
+  `draft`,
+  `active`,
+  `revoked`,
+  `completed`
+];
 
 const generateFHIRServiceRequest = (dataRecord) => {
+  const reportedDate = DateTime.fromMillis(dataRecord.reported_date);
   const FHITServiceRequest = {
+    resourceType: `ServiceRequest`,
+    status: status[0],
+    intent: `order`,
+    category: [
+      {
+        coding: [
+          {
+            system: NHDD_GENERIC_PATH,
+            code: `28414`,
+            display: `Patient referred for medical consultation`
+          }
+        ],
+        text: `Consultation`
+      }
+    ],
+    priority: `urgent`,
+    subject: {
+      reference: `${CLIENT_REGISTRY_URL}/${dataRecord.upi}`,
+      type: `Patient`,
+      display: dataRecord.upi
+    },
+    occurrencePeriod: {
+      resourceType: `Period`,
+      start: reportedDate.toISODate(),
+      end: dataRecord.follow_up_date
+    },
+    authoredOn: reportedDate.toISODate(),
+    requester: {
+      reference: `${NHDD_KMHFL_PATH}/${dataRecord.chu_code}`,
+      type: `Organization`,
+      display: dataRecord.chu_name
+    },
+    performer: [
+      {
+        reference: `${NHDD_KMHFL_PATH}/${dataRecord.referred_to_facility_code}`,
+        type: `Organization`,
+        display: dataRecord.referred_to_facility_name
+      }
+    ],
+    reasonCode: extractReasonCode(dataRecord.screening),
+    note: extractNotes(dataRecord.supportingInfo)
   };
   return FHITServiceRequest;
-};
-
-const saveFHIRServiceRequest = async (dataRecord) => {
-  const FHIRServiceRequest = generateFHIRServiceRequest(dataRecord);
-  const response = await axiosInstance.post('/ServiceRequest', FHIRServiceRequest);
-  return response;
 };
 
 module.exports = {
