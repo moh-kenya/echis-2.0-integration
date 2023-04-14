8b74d6ac560de6d5b809abc28f5563f289792c65
Ezra Mungai <mungaiezrakaruoya@live.com>
2023-04-14 16:01:53 +0300

    Stringfy object

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -31,7 +31,7 @@ const requestNewToken = async () => {
 };
 
 const generateToken = async () => {
-  logger.information(token);
+  logger.information(JSON.stringify(token));
   if (token.timestamp && token.value) {
     const now = new Date();
     const diff = now - token.timestamp;
