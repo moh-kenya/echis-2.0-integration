b88231e8321dc46836aa59251c5e9484c638a4b3
Ezra Mungai <mungaiezrakaruoya@live.com>
2023-04-14 12:57:53 +0300

    [Enhancement]: Adding aggregator logs

--- src/routes/aggregate.js
+++ src/routes/aggregate.js
@@ -1,12 +1,14 @@
 const {Router} = require('express');
 const {getMoh515Data} = require('../controllers/aggregate');
+const {logger} = require('../utils/logger');
 
 const router = Router();
 
 router.get('/run',
   async (req, res) => {
-    console.info("Aggrigator started")
+    logger.information("Aggregator task started");
     await getMoh515Data(req, res);
+    logger.information("Aggregator task completed");
   });
 
 module.exports = router;
