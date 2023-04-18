6b14965f9ae7d64583042e47448a8072cf921d84
Ezra Mungai <mungaiezrakaruoya@live.com>
2023-04-18 15:59:24 +0300

    add comments to logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,21 +1,31 @@
 const logger = {
-
+    /**
+     * Logs a message with the INFO stamp in Green
+     * @param {string} message The message
+     */
     information : (message) => {
         const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
         console.info(`${timestamp} \x1b[32mINFO:\x1b[0m ${message};`);
     },
-
+    /**
+     * Logs a message with the INFO stamp in Orange
+     * @param {string} message 
+     */
     warning : (message) => {
         const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
         console.warn(`${timestamp} \x1b[33mWARNING:\x1b[0m ${message};`);
     },
-
+    /**
+     * Logs a message with the INFO stamp in Red
+     * @param {string} message 
+     */
     error : (message) => {
         const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
         console.error(`${timestamp} \x1b[31mERROR:\x1b[0m ${message};`);
     }
 }
+
+
 module.exports = {
     logger
-};
-  
+};  
