9d8dd24084a9fe9e3f3157f69de37da2b187c4f9
unknown <eric@hisp.org>
2023-08-11 19:32:19 +0300

    Adding mediator_logs.json file and functionality to test connection from cronjob to mediator

--- src/middlewares/aggregate.js
+++ src/middlewares/aggregate.js
@@ -9,9 +9,23 @@ const cronService = () => cron.schedule('0 0 15 * *', () => {
     const res = axios.get(`${MEDIATOR.url}/echis-mediator/aggregate/run`, {
       auth: {username: MEDIATOR.username, password: MEDIATOR.password}
     });
+
+    //checkig if cronjob has been able to connect to mediator
+    var resObject = $.extend(resObject,res);
+    var resString = JSON.stringify(resObject);
+    let connError = false;
+    if(resString.search(/cause: Error: connect ECONNREFUSED/)){
+      connError = true;
+    }
+
+    logger.information("Cronjob cannot connect with the mediator");
+
+    //return the respose object
     return res;
-  } catch (error) {
+  } 
+  catch (error) {
     logger.error(error);
+    logger.information("Error Exporting");
     return error;
   }
 });
