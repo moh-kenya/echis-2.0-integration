f896da6ce5e7dfd64e9705aa42b39a4fc0b0065d
Eric Maira <ericodemus@gmail.com>
2023-08-11 18:36:19 +0200

    Test server changes to middleware aggregate file

--- src/middlewares/aggregate.js
+++ src/middlewares/aggregate.js
@@ -3,14 +3,16 @@ const axios = require('axios');
 const { MEDIATOR } = require('../../config');
 const {logger} =require('../utils/logger');
 
-  const cronService = () => cron.schedule('0 0 5 * *', () => {
-  //const cronService = () => cron.schedule('* * * * *', () => {
+  //const cronService = () => cron.schedule('0 0 5 * *', () => {
+  const cronService = () => cron.schedule('* * * * *', () => {
   logger.information('Running MoH 515 export');
   try {
     const res = axios.get(`${MEDIATOR.url}/echis-mediator/aggregate/run`, {
       auth: {username: MEDIATOR.username, password: MEDIATOR.password}
     });
     logger.information('Export successful');
+    var resObject = $.extend(resObject,res);
+    logger.information(JSON.stringify(resObject));
     return res;
   } catch (error) {
     logger.error(error);
