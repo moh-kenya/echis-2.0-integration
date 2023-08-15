2d3b70e1fd0357544bbe343e958abe5796dbc29b
Karim K. Kanji <karimkkanji@cryosoft.co.ke>
2023-08-15 08:49:54 +0300

    UPD: added auto spawning when server goes down

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const {registerMediator} = require('openhim-mediator-utils');
-const {OPENHIM, CONFIG} = require('./config');
-const clientRoutes = require('./src/routes/client');
-const referralRoutes = require('./src/routes/referral');
-const aggregateRoutes = require('./src/routes/aggregate');
-const {cronService} = require('./src/middlewares/aggregate');
-const {logger} =require('./src/utils/logger');
+const { registerMediator } = require("openhim-mediator-utils");
+const { OPENHIM, CONFIG } = require("./config");
+const clientRoutes = require("./src/routes/client");
+const referralRoutes = require("./src/routes/referral");
+const aggregateRoutes = require("./src/routes/aggregate");
+const { cronService } = require("./src/middlewares/aggregate");
+const { logger } = require("./src/utils/logger");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 logger.information("Setting up routes");
-app.use('/client', clientRoutes);
-app.use('/referral', referralRoutes);
-app.use('/aggregate', aggregateRoutes);
+app.use("/client", clientRoutes);
+app.use("/referral", referralRoutes);
+app.use("/aggregate", aggregateRoutes);
 logger.information("Routes setup complete");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 logger.information("Setting up cron services");
 cronService();
 logger.information("Cron services setup complete");
@@ -32,7 +32,7 @@ const registerMediatorCallback = (err) => {
   if (err) {
     throw new Error(`Mediator Registration Failed: Reason ${err}`);
   }
-  logger.information('Successfully registered mediator.');
+  logger.information("Successfully registered mediator.");
 };
 
 const mediatorConfig = {
@@ -74,9 +74,9 @@ const mediatorConfig = {
 
 registerMediator(OPENHIM, mediatorConfig, registerMediatorCallback);
 
-app.get('/', (req, res) => {
-  logger.information('Loading the root route')
-  res.send('Loaded');
+app.get("/", (req, res) => {
+  logger.information("Loading the root route");
+  res.send("Loaded");
 });
 
 app.get("/", (req, res) => {
