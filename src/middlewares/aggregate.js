91296e2f4c42bfea5ea4e9266d52f61ad8d09ca0
unknown <eric@hisp.org>
2023-08-21 14:46:36 +0300

    Reverting cron job time from 30 seconds to 5th of every month

--- src/middlewares/aggregate.js
+++ src/middlewares/aggregate.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 const { MEDIATOR } = require('../../config');
 const {logger} =require('../utils/logger');
 
-  //const cronService = () => cron.schedule('0 0 5 * *', () => {
-  const cronService = () => cron.schedule('*/30 * * * * *', () => {
+  //const cronService = () => cron.schedule('0 0 15 * *', () => {
+  const cronService = () => cron.schedule('0 0 5 * *', () => {
   logger.information('Running MoH 515 export');
   try {
     //const res = axios.get(`${MEDIATOR.url}/echis-mediator/aggregate/run`, {
