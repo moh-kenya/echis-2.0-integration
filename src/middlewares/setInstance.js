3676ada2a3228fb86706d723d2ca115775685c69
Karim K. Kanji <karimkkanji@cryosoft.co.ke>
2023-10-11 12:39:15 +0300

    UPD: Return 400 instead of 500

--- src/middlewares/setInstance.js
+++ src/middlewares/setInstance.js
@@ -6,7 +6,7 @@ module.exports = function () {
           const clientId = req.headers['x-openhim-clientid'];
             res.locals.instanceValue = clientId;
         } else {
-          res.status(500).send({ error: "Required Headers were not passed"})
+          res.status(400).send({ error: "Required Headers were not passed"})
         }
         next();
       };
