a13da3d6900ebc2eef860f9ee3026cb981840dc4
Karim K. Kanji <karimkkanji@cryosoft.co.ke>
2023-12-15 11:48:05 +0300

    UPD: Updated generatePayload

--- src/utils/ncd.js
+++ src/utils/ncd.js
@@ -6,30 +6,10 @@ const generateDiabetesPayload = (echisDoc) => {
     const nameArray = fields.patient_name?.split(" ");
     let result = {
       gender: capitalizeFirstLetter(fields.patient_sex),
-      bioData: {
-        firstName: fields.first_name || nameArray.at(0),
-        lastName: fields.last_name || nameArray.at(2),
-        country: 1,
-        levelOfEducation: "A-level Completed",
-        phoneNumber: "198765432",
-        occupation: "Employed",
-        nationalId: faker.finance.account(),
-        initial: "E",
-        county: 2,
-        subCounty: 3,
-        insuranceStatus: false,
-        phoneNumberCategory: "Personal"
-      },
-      patientStatus: {
-        htnPatientType: "N/A",
-        diabetesPatientType: "N/A",
-        isHtnDiagnosis: false,
-        isDiabetesDiagnosis: false
-    },
-    isRegularSmoker: false,
-    cvdRiskScore: 2,
-    dateOfBirth: fields.patient_date_of_birth+"T00:00:00+03:00",
-    bpLog: {
+      isRegularSmoker: false,
+      cvdRiskScore: 2,
+      dateOfBirth: fields.patient_date_of_birth+"T00:00:00+03:00",
+      bpLog: {
         avgSystolic: 88,
         weight: fields.weight,
         avgDiastolic: 88,
@@ -53,33 +33,53 @@ const generateDiabetesPayload = (echisDoc) => {
             }
         ],
         bmi: fields.BMI
-    },
-    phq4: {
-        phq4Score: 7,
-        phq4MentalHealth: [
-            {
-                answerId: 67,
-                score: 3,
-                questionId: 6,
-                displayOrder: 4
-            },
-        ],
-        phq4RiskLevel: "Moderate"
-    },
-    cvdRiskLevel: "Low risk",
-    unitMeasurement: "metric",
-    cvdRiskScoreDisplay: "2% - Low risk",
-    bioMetrics: {
-        gender: "Male",
-        isRegularSmoker: false,
+        },
+        bioData: {
+            firstName: fields.first_name || nameArray.at(0),
+            lastName: fields.last_name || nameArray.at(2),
+            country: 4,
+            levelOfEducation: "A-level Completed",
+            phoneNumber: "198765432",
+            occupation: "Employed",
+            nationalId: faker.finance.account(),
+            initial: "E",
+            county: 9,
+            subCounty: 162,
+            insuranceStatus: false,
+            phoneNumberCategory: "Personal"
+        },
+        patientStatus: {
+            htnPatientType: "N/A",
+            diabetesPatientType: "N/A",
+            isHtnDiagnosis: false,
+            isDiabetesDiagnosis: false
+        },
+        phq4: {
+            phq4Score: 7,
+            phq4MentalHealth: [
+                {
+                    answerId: 67,
+                    score: 3,
+                    questionId: 6,
+                    displayOrder: 4
+                },
+            ],
+            phq4RiskLevel: "Moderate"
+        },
+        cvdRiskLevel: "Low risk",
+        unitMeasurement: "metric",
+        cvdRiskScoreDisplay: "2% - Low risk",
+        bioMetrics: {
+            gender: "Male",
+            isRegularSmoker: false,
+            age: Number(fields.patient_age_in_years),
+        },
+        glucoseLog: {
+            glucoseUnit: "mmol/L",
+            hba1cUnit: "mmol/L"
+        },
         age: Number(fields.patient_age_in_years),
-    },
-    glucoseLog: {
-        glucoseUnit: "mmol/L",
-        hba1cUnit: "mmol/L"
-    },
-    age: Number(fields.patient_age_in_years),
-    siteId: 1,
+        siteId: 21,
     };
     return result;
   };
