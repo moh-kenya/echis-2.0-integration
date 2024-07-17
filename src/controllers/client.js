ef6d57762daa79c725b3486730c8c85d33083226
freddieptf <muiru@medic.org>
2024-07-17 16:26:42 +0300

    wip: updated client and referrals route

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -1,169 +1,67 @@
 const axios = require("axios");
-const { generateToken } = require("../utils/auth");
-const { getCHTValuesFromEnv, CLIENT_REGISTRY } = require("../../config");
-const {
-  generateClientRegistryPayload,
-  getMismatchedClientFields,
-} = require("../utils/client");
-const { logger } = require("../utils/logger");
-const { messages } = require("../utils/messages");
-const { updateDoc, createReport } = require("../utils/echis");
-const { CLIENT_DETAILS_MISMATCH, GEN_TOKEN_FAILURE } = messages;
-
-const tryClientRegistryReauth = async (error) => {
-  if (error.response.status !== 401) {
-    return Promise.reject(error);
-  }
-
-  const originalRequest = error.config;
-  if (originalRequest._retry) {
-    return Promise.reject(error);
-  }
-
-  logger.information("Re-authenticating with Client Registry");
-  try {
-    originalRequest._retry = true;
-    const token = await generateToken();
-    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    return axiosInstance(originalRequest);
-  } catch (err) {
-    logger.error(
-      `${GEN_TOKEN_FAILURE} ${err.response.status} ${err.response.body}`
-    );
-  }
-
-  return Promise.reject(error);
-};
+const { CLIENT_REGISTRY } = require("../../config");
+const { getIdentificationType } = require("../utils/client");
+const echis = require("../utils/echis");
 
 const axiosInstance = axios.create({
   baseURL: CLIENT_REGISTRY.url,
-  headers: {
-    "Content-Type": "application/json",
+  auth: {
+    username: CLIENT_REGISTRY.user,
+    password: CLIENT_REGISTRY.pass,
   },
 });
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  tryClientRegistryReauth
-);
 
-// Create client in the Client Registry and update an echis instance with the client number
-const createCRClient = (conf, echisClient) => {
-  const payload = generateClientRegistryPayload(echisClient);
-  return axiosInstance
-    .post(`partners/registry`, payload, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    .then((resp) => {
-      return updateDoc(conf, echisClient._id, {
-        upi: resp.data.clientNumber,
-      }).then(() => Promise.resolve(resp.data.clientNumber));
-    });
-};
-
-const createClientDetailsMismatchReport = (conf, echisClientId, fields) => {
-  const report = {
-    _meta: {
-      form: "CLIENT_DETAILS_MISMATCH",
+const fetchClientFromRegistry = async (contact) => {
+  const params = getIdentificationType(contact);
+  const resp = await axiosInstance.get("/client-registry/fetch-client", {
+    params: {
+      payload: JSON.stringify(params),
     },
-    patient_id: echisClientId,
-    mismatched_fields: fields.join(" "),
-    source: "Client Registry",
-  };
-  return createReport(conf, report);
+  });
+  if (resp.status !== 200 || resp.data.message.total <= 0) {
+    throw new Error(
+      `status: ${resp.status} | matches: ${JSON.stringify(resp.data.message)}`
+    );
+  }
+  if (resp.data.message.total > 1) {
+    throw new Error("multiple matches for id");
+  }
+  return resp.data.message.result[0].id;
 };
 
-async function assignEchisClientUPI(req, res) {
-  const success = (code, echisClientId, clientNumber) => {
-    res.status(code).send({
-      data: {
-        echis_doc: echisClientId,
-        upi: clientNumber,
-      },
-    });
-  };
-  const instance = res.locals.instanceValue;
-  const chtInstanceVariables = getCHTValuesFromEnv(instance);
-  const instanceObject = {
-    instance: chtInstanceVariables.url,
-    user: chtInstanceVariables.username,
-    password: chtInstanceVariables.password,
-  };
-  // if they already exist in CR we check if the fields provided match what is in the CR and if they do, update echis with UPI.
-  if (res.locals.crClientExists) {
-    const mismatchedFields = getMismatchedClientFields(
-      res.locals.echisClient,
-      res.locals.crClient
-    );
-    // if fields do not match, create report on echis for follow up and return error
-    if (Object.keys(mismatchedFields).length > 0) {
-      try {
-        await createClientDetailsMismatchReport(
-          instanceObject,
-          res.locals.echisClient._id,
-          Object.keys(mismatchedFields)
-        );
-        res.status(200).send({
-          data: {
-            msg: CLIENT_DETAILS_MISMATCH,
-            fields: mismatchedFields,
-          },
-        });
-      } catch (err) {
-        const errString = `could not create details mismatch report on ${
-          getCHTValuesFromEnv(instance).url
-        }: ${err.message}`;
-        logger.error(errString);
-        res.status(400).send({ error: errString });
-      }
-    } else {
-      // else update contact doc with UPI
-      try {
-        await updateDoc(instanceObject, res.locals.echisClient._id, {
-          upi: res.locals.crClient.clientNumber,
-        });
-        success(
-          200,
-          res.locals.echisClient._id,
-          res.locals.crClient.clientNumber
-        );
-      } catch (err) {
-        const errString = `could not update doc ${
-          res.locals.echisClient._id
-        } on ${getCHTValuesFromEnv(instance).url}: ${err.message}`;
-        logger.error(errString);
-        res.status(400).send({ error: errString });
-      }
-    }
-    return; // nothing more to do here
+const updateContactCRID = async (instance, contact) => {
+  let id = await fetchClientFromRegistry(contact);
+  await echis.updateDoc(echis.getInstanceConf(instance), contact._id, {
+    client_registry: {
+      id,
+      status: "OK",
+    },
+  });
+  return id;
+};
+
+const contactHandler = async (_, response) => {
+  const contact = response.locals.contact;
+  if (contact.client_registry?.status === "OK") {
+    return;
   }
-  // echis client did not exist in the registry, lets create them
   try {
-    const clientNumber = await createCRClient(
-      instanceObject,
-      res.locals.echisClient
+    await updateContactCRID(response.locals.instanceValue, contact);
+  } catch (error) {
+    await echis.updateDoc(
+      echis.getInstanceConf(response.locals.instanceValue),
+      contact._id,
+      {
+        client_registry: {
+          error: error.message,
+        },
+      }
     );
-    success(201, res.locals.echisClient._id, clientNumber);
-  } catch (err) {
-    const errStruct = {
-      instance: getCHTValuesFromEnv(instance).url,
-      user: res.locals.echisClient._id,
-      error:
-        err?.response?.data?.errors ||
-        err?.message ||
-        "Could not create Client in CR",
-    };
-    const errString = `could not complete creating client ${JSON.stringify(
-      errStruct
-    )})`;
-    logger.error(errString);
-    res.status(400).send({ error: errString });
   }
-}
+};
 
 module.exports = {
-  assignEchisClientUPI,
-  crAxiosInstance: axiosInstance,
-  createCRClient,
+  fetchClientFromRegistry,
+  updateContactCRID,
+  contactHandler,
 };
--- src/middlewares/setClient.js
+++ src/middlewares/setClient.js
@@ -1,28 +1,15 @@
 const { getIdentificationType } = require("../utils/client");
 const { logger } = require("../utils/logger");
 
-module.exports = function (axiosInstance) {
-  return async function setClient(req, res, next) {
-    const contactDoc = req.body?.doc;
-    if (!contactDoc) {
-      res.status(400).send("Contact Doc Empty");
-      return;
-    }
-    if (contactDoc?.upi) {
-      res.status(304).send();
-      return;
-    }
-
-    res.locals.echisClient = contactDoc;
-    const idType = getIdentificationType(contactDoc.identification_type);
-    const crSearchURL = `partners/registry/search/${idType}/${contactDoc.identification_number}`;
-    try {
-      const response = await axiosInstance.get(crSearchURL);
-      res.locals.crClientExists = response.data?.clientExists;
-      res.locals.crClient = response.data?.client;
-    } catch (err) {
-      logger.error(`Error while searching Client Registry: ${err.message}`);
-      // throw err;
+module.exports = function () {
+    return async function setClient(req, res, next) {
+        const contact = req.body?.doc;
+        if (!contact) {
+            res.status(400).send("Contact Doc Empty");
+            return
+        }
+        res.locals.contact = contact;
+        next()
     }
 
     next();
