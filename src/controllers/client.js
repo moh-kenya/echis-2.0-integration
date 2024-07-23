aad540cb0ebfc737ed24aeec24478e40221902cb
freddieptf <muiru@medic.org>
2024-07-23 19:19:58 +0300

    move to new dir

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -1,72 +1,166 @@
 const axios = require("axios");
-const { TIBERBU_SERVICE } = require("../../config");
-const { getIdentificationType } = require("../utils/client");
-const echis = require("../utils/echis");
+const { generateToken } = require("../utils/auth");
+const { getCHTValuesFromEnv, CLIENT_REGISTRY } = require("../../config");
+const {
+  generateClientRegistryPayload,
+  getMismatchedClientFields,
+} = require("../utils/client");
 const { logger } = require("../utils/logger");
+const { messages } = require("../utils/messages");
+const { updateDoc, createReport } = require("../utils/echis");
+const { CLIENT_DETAILS_MISMATCH, GEN_TOKEN_FAILURE } = messages;
 
-const axiosInstance = axios.create({
-  baseURL: TIBERBU_SERVICE.url,
-  auth: {
-    username: TIBERBU_SERVICE.user,
-    password: TIBERBU_SERVICE.pass,
-  },
-});
-
-const checkErr = (err, resp) => {
-  let issues = resp.data.message?.issue || resp.data.issue;
-  let errString = issues.map((issue) => issue.diagnostics).join(",");
-  if (errString.includes("not found")) {
-    throw new Error("no matches");
+const tryClientRegistryReauth = async (error) => {
+  if (error.response.status !== 401) {
+    return Promise.reject(error);
   }
-  if (err) throw err;
-};
 
-const fetchClientFromRegistry = async (contact) => {
-  const params = getIdentificationType(contact);
-  let resp;
+  const originalRequest = error.config;
+  if (originalRequest._retry) {
+    return Promise.reject(error);
+  }
+
+  logger.information("Re-authenticating with Client Registry");
   try {
-    resp = await axiosInstance.get("/api/v4/Patient", {
-      params: { ...params },
-    });
+    originalRequest._retry = true;
+    const token = await generateToken();
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    return axiosInstance(originalRequest);
   } catch (err) {
-    checkErr(err, err.response);
-  }
-  return resp.data.id || checkErr(resp);
-};
-
-const updateContactCRID = async (instance, contact) => {
-  let id = await fetchClientFromRegistry(contact);
-  await echis.updateDoc(echis.getInstanceConf(instance), contact._id, {
-    client_registry: {
-      id,
-      status: "OK",
-    },
-  });
-  return id;
-};
-
-const contactHandler = async (_, response) => {
-  const contact = response.locals.contact;
-  if (contact.client_registry?.status === "OK") {
-    return;
-  }
-  try {
-    await updateContactCRID(response.locals.instanceValue, contact);
-  } catch (error) {
-    await echis.updateDoc(
-      echis.getInstanceConf(response.locals.instanceValue),
-      contact._id,
-      {
-        client_registry: {
-          error: error.message,
-        },
-      }
+    logger.error(
+      `${GEN_TOKEN_FAILURE} ${err.response.status} ${err.response.body}`
     );
   }
+
+  return Promise.reject(error);
 };
 
+const axiosInstance = axios.create({
+  baseURL: CLIENT_REGISTRY.url,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  tryClientRegistryReauth
+);
+
+// Create client in the Client Registry and update an echis instance with the client number
+const createCRClient = (conf, echisClient) => {
+  const payload = generateClientRegistryPayload(echisClient);
+  return axiosInstance
+    .post(`partners/registry`, payload, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((resp) => {
+      return updateDoc(conf, echisClient._id, {
+        upi: resp.data.clientNumber,
+      }).then(() => Promise.resolve(resp.data.clientNumber));
+    });
+};
+
+const createClientDetailsMismatchReport = (conf, echisClientId, fields) => {
+  const report = {
+    _meta: {
+      form: "CLIENT_DETAILS_MISMATCH",
+    },
+    patient_id: echisClientId,
+    mismatched_fields: fields.join(" "),
+    source: "Client Registry",
+  };
+  return createReport(conf, report);
+};
+
+async function assignEchisClientUPI(req, res) {
+  const success = (code, echisClientId, clientNumber) => {
+    res.status(code).send({
+      data: {
+        echis_doc: echisClientId,
+        upi: clientNumber,
+      },
+    });
+  };
+  const instance = res.locals.instanceValue;
+  const chtInstanceVariables = getCHTValuesFromEnv(instance);
+  const instanceObject = {
+    instance: chtInstanceVariables.url,
+    user: chtInstanceVariables.username,
+    password: chtInstanceVariables.password,
+  };
+  // if they already exist in CR we check if the fields provided match what is in the CR and if they do, update echis with UPI.
+  if (res.locals.crClientExists) {
+    const mismatchedFields = getMismatchedClientFields(
+      res.locals.echisClient,
+      res.locals.crClient
+    );
+    // if fields do not match, create report on echis for follow up and return error
+    if (Object.keys(mismatchedFields).length > 0) {
+      try {
+        await createClientDetailsMismatchReport(
+          instanceObject,
+          res.locals.echisClient._id,
+          Object.keys(mismatchedFields)
+        );
+        res.status(200).send({
+          data: {
+            msg: CLIENT_DETAILS_MISMATCH,
+            fields: mismatchedFields,
+          },
+        });
+      } catch (err) {
+        const errString = `could not create details mismatch report on ${
+          getCHTValuesFromEnv(instance).url
+        }: ${err.message}`;
+        logger.error(errString);
+        res.status(400).send({ error: errString });
+      }
+    } else {
+      // else update contact doc with UPI
+      try {
+        await updateDoc(instanceObject, res.locals.echisClient._id, {
+          upi: res.locals.crClient.clientNumber,
+        });
+        success(
+          200,
+          res.locals.echisClient._id,
+          res.locals.crClient.clientNumber
+        );
+      } catch (err) {
+        const errString = `could not update doc ${
+          res.locals.echisClient._id
+        } on ${getCHTValuesFromEnv(instance).url}: ${err.message}`;
+        logger.error(errString);
+        res.status(400).send({ error: errString });
+      }
+    }
+    return; // nothing more to do here
+  }
+  // echis client did not exist in the registry, lets create them
+  try {
+    const clientNumber = await createCRClient(
+      instanceObject,
+      res.locals.echisClient
+    );
+    success(201, res.locals.echisClient._id, clientNumber);
+  } catch (err) {
+    const errStruct = {
+      instance: getCHTValuesFromEnv(instance).url,
+      user: res.locals.echisClient._id,
+      error: err.response.data.errors || err.message,
+    };
+    const errString = `could not complete creating client ${JSON.stringify(
+      errStruct
+    )})`;
+    logger.error(errString);
+    res.status(400).send({ error: errString });
+  }
+}
+
 module.exports = {
-  fetchClientFromRegistry,
-  updateContactCRID,
-  contactHandler,
+  assignEchisClientUPI,
+  crAxiosInstance: axiosInstance,
+  createCRClient,
 };
---
+++ src/controllers/v2/client.js
@@ -0,0 +1,72 @@
+const axios = require("axios");
+const { TIBERBU_SERVICE } = require("../../../config");
+const { getIdentificationType } = require("../../utils/client");
+const echis = require("../../utils/echis");
+const { logger } = require("../../utils/logger");
+
+const axiosInstance = axios.create({
+  baseURL: TIBERBU_SERVICE.url,
+  auth: {
+    username: TIBERBU_SERVICE.user,
+    password: TIBERBU_SERVICE.pass,
+  },
+});
+
+const checkErr = (resp, err) => {
+  let issues = resp.data.message?.issue || resp.data.issue;
+  let errString = issues.map((issue) => issue.diagnostics).join(",");
+  if (errString.includes("not found")) {
+    throw new Error("no matches");
+  }
+  if (err) throw err;
+};
+
+const fetchClientFromRegistry = async (contact) => {
+  const params = getIdentificationType(contact);
+  let resp;
+  try {
+    resp = await axiosInstance.get("/api/v4/Patient", {
+      params: { ...params },
+    });
+  } catch (err) {
+    checkErr(err.response, err);
+  }
+  return resp.data.id || checkErr(resp);
+};
+
+const updateContactCRID = async (instance, contact) => {
+  let id = await fetchClientFromRegistry(contact);
+  await echis.updateDoc(echis.getInstanceConf(instance), contact._id, {
+    client_registry: {
+      id,
+      status: "OK",
+    },
+  });
+  return id;
+};
+
+const contactHandler = async (_, response) => {
+  const contact = response.locals.contact;
+  if (contact.client_registry?.status === "OK") {
+    return;
+  }
+  try {
+    await updateContactCRID(response.locals.instanceValue, contact);
+  } catch (error) {
+    await echis.updateDoc(
+      echis.getInstanceConf(response.locals.instanceValue),
+      contact._id,
+      {
+        client_registry: {
+          error: error.message,
+        },
+      }
+    );
+  }
+};
+
+module.exports = {
+  fetchClientFromRegistry,
+  updateContactCRID,
+  contactHandler,
+};
