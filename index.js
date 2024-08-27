957565462f8baaa1a37a5690adc9b4fcfb9da774
freddieptf <muiru@medic.org>
2024-08-27 17:01:34 +0300

    add top level routes

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const morgan = require("morgan");
 
-const { registerMediator } = require("openhim-mediator-utils");
-const {
-  OPENHIM,
-  CONFIG,
-  CHANNEL_CONFIG_ENDPOINTS_URL,
-  CRON_SCHEDULE,
-  MEDIATOR,
-} = require("./config");
-const clientRoutes = require("./src/routes/client");
-const referralRoutes = require("./src/routes/referral");
-const aggregateRoutes = require("./src/routes/aggregate");
-const { scheduleTask } = require("./src/cron/cron");
-const { logger } = require("./src/utils/logger");
-const { messages } = require("./src/utils/messages");
+const { registerMediator } = require('openhim-mediator-utils');
+const {OPENHIM, CONFIG, CHANNEL_CONFIG_ENDPOINTS_URL, CRON_SCHEDULE, MEDIATOR} = require('./config');
+const clientRoutes = require('./src/routes/client');
+const clientv2Routes = require('./src/routes/v2/client');
+const referralRoutes = require('./src/routes/referral');
+const referralv2Routes = require('./src/routes/v2/referral');
+const aggregateRoutes = require('./src/routes/aggregate');
+const { scheduleTask } = require('./src/cron/cron');
+const { logger } = require('./src/utils/logger');
+const { messages } = require('./src/utils/messages');
 
 const {
   ROUTES_SETUP,
@@ -32,22 +29,21 @@ const {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
 logger.information(ROUTES_SETUP);
 
 app.use("/client", clientRoutes);
+app.use("/v2/client", clientv2Routes);
 app.use("/referral", referralRoutes);
+app.use("/v2/referral", referralv2Routes);
 app.use("/aggregate", aggregateRoutes);
 
 logger.information(ROUTES_SETUP_COMPLETE);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Disabled CRON since KHIS<>eCHIS integration was moved to NiFi
-// logger.information(CRON_SETUP);
-// scheduleTask(CRON_SCHEDULE, MEDIATOR);
-// logger.information(CRON_SETUP_COMPLETE);
+logger.information(CRON_SETUP);
+scheduleTask(CRON_SCHEDULE, MEDIATOR);
+logger.information(CRON_SETUP_COMPLETE);
 
 app.listen(CONFIG.port, () => {
   logger.information(`${SERVER_PORT} ${CONFIG.port}`);
